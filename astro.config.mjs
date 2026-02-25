1d7480a2d4460776b6563a0f3a556f840b0ced3a
kolibrek <kolibrek@users.noreply.github.com>
2026-02-24 18:25:19 -0700

    Initial Styling
    Added UnoCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import UnoCSS from '@unocss/astro'
+import { presetWind3 } from 'unocss';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+      presets: [presetWind3()],
+    }),
+  ],
+});
