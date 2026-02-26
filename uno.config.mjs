9a3afd6c3d12e64c69d5da9db740a60c3f586f0f
kolibrek <kolibrek@users.noreply.github.com>
2026-02-26 12:10:26 -0700

    renamed components folder

--- uno.config.mjs
+++ uno.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'unocss';
 
 export default defineConfig({
-
+  shortcuts: [
+    {
+      'btn': 'border-none uppercase text-center font-700 cursor-pointer',
+      'scale': 'hover:scale-110 will-change-transform'
+    },
+    [/^btn-(.*)$/, ([,c]) => `bg-${c}-500 text-${c}-100 hover:bg-${c}-600 py-2 px-4 transition`],
+  ]
 });
